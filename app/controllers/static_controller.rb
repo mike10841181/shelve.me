051d9d77aa0fef2e89a329e717c9c53f4aa8086d
Mike Andino <mikeandinog@MIKE-laptop.(none)>
2010-11-15 13:52:57 -0600

    problemas con el testhelper y ademas con los controladores atuda porfa

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class StaticControllerTest < ActionController::TestCase
+      test "should get index" do
+        get :index
+        assert_response :success
+        assert_select "a[href=/users/sign_in]", :text=>"Login"
+        assert_select "a[href=/users/sign_up]", :text=>"Register"
+      end
+    end
