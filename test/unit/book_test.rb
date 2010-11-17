0d3eb93f7524011e7ac4f176b4f1710ee2466a6f
Mike Andino <mikeandinog@MIKE-laptop.(none)>
2010-11-16 23:01:39 -0600

    llegue hasta donde me dio jejeje

--- test/unit/book_test.rb
+++ test/unit/book_test.rb
@@ -5,6 +5,9 @@ class BookTest < ActiveSupport::TestCase
   test "book gets created" do 
     b = Book.new(:author=>"E.A. Poe", :title=>"The imp of the perverse", :description=>"essay on the human condition")
     assert b.save
+
+    c = Book.new(:author=>"Gaahl", :title=>"Gaahlism")
+    assert c.save
   end
 
   test "title can't be empty" do 
@@ -27,33 +30,87 @@ class BookTest < ActiveSupport::TestCase
   end
   
   test "author is capitalized" do 
-    b = Book.new(:title=>"another book", :author=>"ARTHUR C. CLARKE")
+    b = Book.new(:title=>"another book", :author=>"ARtHuR C. ClARkE")
     assert b.save
     assert_equal "Arthur C. Clarke", b.author
+  end
+
+  test "author is stored with stripped space" do 
+    b = Book.new :title=>"The Gods Themselves", :author=>"Arthur C.    Clarke   "
+    assert b.save
+    assert_equal "Arthur C. Clarke", b.author
+  end
+
+  test "title is capitalized" do 
+    b = Book.new :title=>"2001: A SpAcE OdYsSeY", :author=>"Arthur C. Clarke"
+    assert b.save
+    assert_equal "2001: A Space Odyssey", b.title
+  end
+
+  test "title is stored with extra space stripped" do
+    title = "  The     life  and  times of    James Taggart  "
+    b = Book.new(:title=>title, :author=>"Dagny Taggart")
+    
+    assert b.save
+    assert_equal "The Life And Times Of James Taggart", b.title
   end
 
   test "title and author can't be duplicated" do 
     a = books :one
     #a completely duplicated book is not allowed
+    #cf: http://siddharthdawara.blogspot.com/2008/09/rails-beforesave-and-validations.html
     b = Book.new(:title=>a.title, :author=>a.author)
-    assert !b.save
+    assert_equal false, b.save 
     assert b.errors[:title].any?
     
     #validation MUST be case insensitive
     c = Book.new(:title=>a.title.upcase, :author=>a.author)
-    assert !c.save
+    
+    assert_equal false, c.valid?
     assert c.errors[:title].any?
 
     #a book with a different author should be allowed
     d = Book.new(:title=>a.title, :author=>"Another author")
+
     assert d.save
+    
+    #slightly different books that are, nevertheless, equal, shouldn't be allowed
+    e = Book.create!(:title => "I, Robot", :author=>"Isaac Asimov")
+    f = Book.new :title=>"I,   ROBOT", :author=>"iSAaC    aSImOV"
+
+    assert_equal false, f.valid?
   end
 
-  test "title is stored with extra space stripped" do
-    title = "The     life  and  times of    James Taggart"
-    b = Book.new(:title=>title, :author=>"Dagny Taggart")
+  test "readers are added" do 
+    book = Book.create! :title=>"The Catcher in the Rye", :author=>"J.D. Salinger"
+    u = users(:one)
+    v = users(:two)
+    #esta es una asociación has_and_belongs_to_many:
+    #cómo declararla: http://guides.rubyonrails.org/association_basics.html#the-has_and_belongs_to_many-association
+    #cómo cambiar el nombre de la relación: http://guides.rubyonrails.org/association_basics.html#has_and_belongs_to_many-class_name
+    #cómo aplicarla a la base de datos: http://guides.rubyonrails.org/association_basics.html#creating-join-tables-for-has_and_belongs_to_many-associations
+    #cómo usarla: http://guides.rubyonrails.org/association_basics.html#has_and_belongs_to_many-association-reference
     
-    assert b.save
-    assert_equal "The life and times of James Taggart", b.title
+    assert_difference("book.readers.count", 2) do
+      book.readers << u
+      book.readers << v
+    end
+
+    assert book.readers.exists? :username => u.username 
+    assert book.readers.exists? :username => v.username 
   end
+
+  test "readers are deleted" do 
+    book = Book.create! :title => "El aciago demiurgo", :author=>"Émile Michel Cioran"
+    
+    assert_difference("book.readers.count") do
+      book.readers << users(:one)
+    end
+
+    assert_difference("book.readers.count", -1) do 
+      book.readers.delete users(:one)
+    end
+    
+  end
+
 end
