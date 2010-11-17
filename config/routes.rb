88745931cad72b8064fff9b83302e8a394b61b4d
Mike Andino <mikeandinog@MIKE-laptop.(none)>
2010-11-17 14:17:33 -0600

    pasar a lo de la clase en master

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 ShelveMe::Application.routes.draw do
-  devise_for :users
 
-  get "static/index"
+  devise_for :users
+  resources(:books,:only=> [:index,:show,:new,:create] ) do
+	get 'search', :on=> :collection
+   end
+ 
 
   root :to => "static#index"
 end
