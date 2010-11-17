988eff132d66ecc5c1709014da8e2c6cc9b47f52
Mike Andino <mikeandinog@MIKE-laptop.(none)>
2010-11-17 07:33:22 -0600

    creacion de branch clase

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101116134622) do
+ActiveRecord::Schema.define(:version => 20101117044620) do
+
+  create_table "books", :force => true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
