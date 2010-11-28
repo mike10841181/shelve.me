42f1375aef064361540e2c8ca0eaba6842a5d5cb
Mike Andino <mikeandinog@MIKE-laptop.(none)>
2010-11-28 16:58:18 -0600

    hasta aqui funciona bn el show y estoy comenzando mpdificar el controller fue ultimo q hice d shoe

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,9 +18,27 @@ class BooksController < ApplicationController
   end
 
   def show
+      @book = Book.find params[:id]
   end
 
   def search
+  end
+  def update
+    #podrías llegar acá con
+    #<%=link_to "read this book", [current_user, @book], :method=>"put"%>
+    @user  = User.find params[:user_id]
+    @book = Book.find params[:id]
+    @user.read @book
+    redirect_to @book
+  end
+
+  def destroy 
+    #podrías llegar acá con
+    #<%=link_to "forget this book", [current_user, @book], :method=>"delete"%>
+    @user  = User.find params[:user_id]
+    @book = Book.find params[:id]
+    @user.forget @book
+    redirect_to @book 
   end
 
 end
